8cb0759661eedecfe13871a88d4229cc5e27f2a9
Tejaswi Gowda <tejaswi@asu.edu>
2023-05-02 22:32:54 -0700

    ble reconnect added

--- script.js
+++ script.js
@@ -29,6 +29,49 @@ const debounce = (func, wait) => {
   };
 };
 
+async function connect_aux() {
+  exponentialBackoff(3 /* max retries */, 2 /* seconds delay */,
+    async function toTry() {
+      time('Connecting to Bluetooth Device... ');
+      await bluetoothDevice.gatt.connect();
+    },
+    function success() {
+      console.log('> Bluetooth Device connected. Try disconnect it now.');
+    },
+    function fail() {
+      time('Failed to reconnect.');
+    });
+}
+
+function onDisconnected() {
+  console.log('> Bluetooth Device disconnected');
+  connect_aux();
+}
+
+/* Utils */
+
+// This function keeps calling "toTry" until promise resolves or has
+// retried "max" number of times. First retry has a delay of "delay" seconds.
+// "success" is called upon success.
+async function exponentialBackoff(max, delay, toTry, success, fail) {
+  try {
+    const result = await toTry();
+    success(result);
+  } catch(error) {
+    if (max === 0) {
+      return fail();
+    }
+    time('Retrying in ' + delay + 's... (' + max + ' tries left)');
+    setTimeout(function() {
+      exponentialBackoff(--max, delay * 2, toTry, success, fail);
+    }, delay * 1000);
+  }
+}
+
+function time(text) {
+  log('[' + new Date().toJSON().substr(11, 8) + '] ' + text);
+}
+
 async function connect() {
   try {
     manageModal.close();
@@ -38,7 +81,14 @@ async function connect() {
       filters: [
         { namePrefix: "MM-" },
       ]
-    }).catch(error => { console.log(error); });;
+    }).catch(error => { console.log(error); });
+      
+    device.addEventListener('gattserverdisconnected', onDisconnected);
+    connect_aux();
+
+  
+
+
     console.log('Connected to device : ', device.name);
 
 
