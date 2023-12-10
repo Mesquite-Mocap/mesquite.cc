d236f5ea7518c6d31793746606a84611c1181733
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-10 08:08:59 -0700

    ac

--- threejsscene.js
+++ threejsscene.js
@@ -167,8 +167,8 @@ import { OrbitControls } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesqui
 import { GLTFLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc/build/GLTFLoader.js";
 import { KTX2Loader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc/build/KTX2Loader.js";
 import { MeshoptDecoder } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/meshopt_decoder.module.js";
-//import { FBXLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/FBXLoader.js";
-import { FBXLoader } from "./build/FBXLoader.js";
+import { FBXLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/FBXLoader.js";
+//import { FBXLoader } from "./build/FBXLoader.js";
 import { BVHLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/BVHLoader.js";
 // import {BVHLoader} from "./build/BVHLoader.js"
 
