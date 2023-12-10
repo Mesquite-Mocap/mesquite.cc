bf5fceccf2fd633960c7836a4d293fe24c25d9bf
Tejaswi Gowda <tejaswi@asu.edu>
2023-12-09 18:56:35 -0700

    ac

--- threejsscene.js
+++ threejsscene.js
@@ -76,7 +76,8 @@ async function predictFace() {
 import * as THREE from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/three.module.js";
 import Stats from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/stats.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/GLTFLoader.js";
+import {GLTFLoader} from "./build/GLTFLoader.js";
+// import { GLTFLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/GLTFLoader.js";
 //import { KTX2Loader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/KTX2Loader.js";
 //import { MeshoptDecoder } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/meshopt_decoder.module.js";
 import { FBXLoader } from "https://cdn.jsdelivr.net/gh/mesquite-mocap/mesquite.cc@latest/build/FBXLoader.js";
