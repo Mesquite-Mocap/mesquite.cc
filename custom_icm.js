fb8a0f62e312ab18f89fbc1cab1e43cabacae0df
darshp1406@gmail.com <darshp1406@gmail.com>
2024-10-15 11:06:14 -0700

    LeftForearm done

--- custom_icm.js
+++ custom_icm.js
@@ -101,7 +101,7 @@ function handleWSMessage(obj) {
     var refQInverse = new THREE.Quaternion().copy(refQuaternion).invert();
     var transformedQ = new THREE.Quaternion().multiplyQuaternions(refQInverse, rawQuaternion);
     var q = new THREE.Quaternion().copy(transformedQ);
-    var armQ = new THREE.Quaternion(-q.y, q.z, -q.x, q.w).normalize();
+    var armQ = new THREE.Quaternion(-q.x, -q.z, -q.y, q.w).normalize();
     
     var spine = model.getObjectByName(rigPrefix + "Spine");
     var spineQ = new THREE.Quaternion().copy(spine.quaternion);
@@ -109,6 +109,22 @@ function handleWSMessage(obj) {
     var spineCorrection = new THREE.Quaternion().copy(spineQinverse).multiply(armQ).normalize();
 
     x.quaternion.copy(spineCorrection);
+  }
+
+  if (bone == "LeftForeArm") {
+    
+    var refQInverse = new THREE.Quaternion().copy(refQuaternion).invert();
+    var transformedQ = new THREE.Quaternion().multiplyQuaternions(refQInverse, rawQuaternion);
+    var q = new THREE.Quaternion().copy(transformedQ);
+    var armQ = new THREE.Quaternion(-q.x, -q.z, -q.y, q.w).normalize();
+    
+    var spine = model.getObjectByName(rigPrefix + "Spine");
+    var spineQ = new THREE.Quaternion().copy(spine.quaternion);
+    var spineQinverse = new THREE.Quaternion().copy(spineQ).invert();
+    var spineCorrection = new THREE.Quaternion().copy(spineQinverse).multiply(armQ).normalize();
+
+    x.quaternion.copy(spineCorrection);
+    
   }
 
   if (bone == "RightArm") {
@@ -141,6 +157,7 @@ function handleWSMessage(obj) {
     x.quaternion.copy(spineCorrection);
     
   }
+
   if (!mac2Bones[bone]) {
     mac2Bones[bone] = {};
     mac2Bones[bone].last = { x: 0, y: 0, z: 0, w: 1 };
