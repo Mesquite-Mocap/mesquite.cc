dff4a5930a0ac3fe0d9054a090d3eb2aa22a0a91
Tejaswi Gowda <tejaswi@asu.edu>
2024-11-16 23:05:30 -0700

    ac

--- js/custom_icm.js
+++ js/custom_icm.js
@@ -137,6 +137,7 @@ function calibrate() {
 
   M.Toast.dismissAll();
   M.toast({ html: "T-Pose Set!", displayLength: 5000, classes: "green" });
+  M.toast({ html: "All set to go! Good luck with your capture.", displayLength: 5000, classes: "" });
 }
 
 function lowerFirstLetter(string) {
@@ -663,22 +664,19 @@ var boneSelectMarkup =
 
 function restartPods()
 {
-  var x = confirm("Please be wearing the pods and get in a T-pose for 25 seconds");
-  if (x){
-    M.Toast.dismissAll();
-    restartPodsConfirm();
-  }
+  M.Toast.dismissAll();
+  M.toast({html: 'Please wear the pods and get in a T-pose for 30 seconds.<br> <button class="btn-flat toast-action green" style="margin-right:20px" onclick="M.Toast.dismissAll();restartPodsConfirm()">Continue</button>', classes: 'yellow black-text', displayLength: 10000});
 }
 function restartPodsConfirm(){
     $("#restartPods").prop('disabled', true);
     window.sWrite("reboot");
-    M.toast({html: '<ul><li>Please get in a T-pose and  wait for <span class="secs">25 seconds</span>.</li><li>When done the T-Pose* will be set.</li><li><sub>* You can click on "Set T-Pose" button to do this at anytime.</sub></li>', classes: 'yellow black-text', displayLength: 25*1000});
+    M.toast({html: '<ul><li>Please get in a T-pose and  wait for <span class="secs" style="font-size:200%;font-weight:bold  ">25 seconds</span>.</li><li>When done the T-Pose* will be set.</li><li><sub>* You can click on "Set T-Pose" button to do this at anytime.</sub></li>', classes: 'yellow black-text', displayLength: 30*1000});
     setTimeout(function(){
      calibrate();
      $("#restartPods").prop('disabled', false);
-    }, 25*1000);
+    }, 30*1000);
 
-    var tSec = 25;
+    var tSec = 30;
     setInterval(function(){
       var secs = document.getElementsByClassName("secs")[0];
       if (secs){
@@ -689,6 +687,9 @@ function restartPodsConfirm(){
         else{
           secs.innerHTML = tSec + " seconds";
         }
+      }
+      else{
+        clearInterval();
       }
     }, 1000);
 }
