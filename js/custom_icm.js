dbc1b3228775ce7b9c90ea53169a4c48c8f09062
Tejaswi Gowda <tejaswi@asu.edu>
2025-01-03 21:13:36 -0700

    ac

--- js/custom_icm.js
+++ js/custom_icm.js
@@ -802,6 +802,7 @@ function restartPodsConfirm(){
 function calibratein5()
 {
   M.Toast.dismissAll();
+  M.toast({html: 'T-Pose Calibration', classes: 'white black-text toastheader', displayLength: 10000});
   M.toast({html: 'Please wear the pods and get in a T-pose for 5 seconds.<br> <button class="btn-flat toast-action green" style="margin-right:20px" onclick="M.Toast.dismissAll();calibratein5Confirm()">Start Timer</button>', classes: 'yellow black-text', displayLength: 10000});
 }
 
@@ -822,8 +823,8 @@ function skipBoxCalibrate(){
 
 function boxCalibrateIn30(){
   M.Toast.dismissAll();
-  M.toast({html: 'BEGIN BOX CALIBRATION<br>', classes: 'white black-text toastheader', displayLength: 10000});
-  M.toast({html: '<p> Please turn on all pods in the box before continuing....</p><button class="btn-flat toast-action green" style="margin:4px" onclick="M.Toast.dismissAll();boxCalibratein30Confirm()">Start</button><button class="btn-flat toast-action blue" style="margin:4px" onclick="M.Toast.dismissAll();quickBoxCalibrate()">Quick Calibrate</button><button class="btn-flat toast-action black" style="margin:4px" onclick="M.Toast.dismissAll();skipBoxCalibrate()">Skip</button><button class="btn-flat toast-action red" style="margin:4px" onclick="M.Toast.dismissAll()">Cancel</button>', classes: 'yellow black-text', displayLength: 10000});
+  M.toast({html: 'BOX CALIBRATION', classes: 'white black-text toastheader', displayLength: 10000});
+  M.toast({html: ' Please turn on all pods in the box before continuing....<button class="btn-flat toast-action green" style="margin:4px" onclick="M.Toast.dismissAll();boxCalibratein30Confirm()">Start</button><button class="btn-flat toast-action blue" style="margin:4px" onclick="M.Toast.dismissAll();quickBoxCalibrate()">Quick Calibrate</button><button class="btn-flat toast-action black" style="margin:4px" onclick="M.Toast.dismissAll();skipBoxCalibrate()">Skip</button><button class="btn-flat toast-action red" style="margin:4px" onclick="M.Toast.dismissAll()">Cancel</button>', classes: 'yellow black-text', displayLength: 10000});
 }
 
 function boxCalibratein30Confirm(){
