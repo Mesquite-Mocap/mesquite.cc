45e22d4b04f3933f11bbcdd4ba713d8417749226
Tejaswi Gowda <tejaswi@asu.edu>
2025-05-30 17:12:51 -0700

    ac

--- js/bvh_converter.js
+++ js/bvh_converter.js
@@ -156,7 +156,7 @@ function generateBVH(jointInfo, motionData) {
 
     // Add the MOTION section with the appropriate number of frames and frame time
     numFramesrecorded = motionData.length;
-    frameTime = 1/40;
+    frameTime = 1/30;
     console.log(numFramesrecorded);
     bvhContent += "MOTION\n";
     bvhContent += `Frames: ${numFramesrecorded}\n`;
