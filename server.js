cc4f1c7796b71a4085331bcb1d515f5a39579e1e
Tejaswi Gowda <tejaswigowda@Tejaswis-M1-MacBook-Pro.local>
2026-05-04 16:52:20 -0700

    ac

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
 //server.use(bodyParser());
+server.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 server.use(express.static(__dirname + '/'));
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
