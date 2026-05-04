a34b42dea31ec79fcf684fcf72ea43786ebb4854
Tejaswi Gowda <tejaswigowda@Tejaswis-M1-MacBook-Pro.local>
2026-05-04 16:59:05 -0700

    ac

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var server = express();
-//var bodyParser = require('body-parser');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
-//server.use(bodyParser());
 server.use((req, res, next) => {
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   res.set('Pragma', 'no-cache');
@@ -14,8 +12,6 @@ server.use((req, res, next) => {
 server.use(express.static(__dirname + '/'));
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-
-//server.listen(port);
 
 if (require.main === module) { server.listen(port); }// Instead do export the app:
 else{ module.exports = server; }
